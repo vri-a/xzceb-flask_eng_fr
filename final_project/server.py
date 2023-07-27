fd823cf03133dcb666b02a1b815fb924650045c3
vri-a <1ijvaleriaperez@gmail.com>
2023-07-27 17:22:11 -0400

    Revert "Task push"
    This reverts commit 48a7f5cddfd8f5349b76e1fde74eaccc4c4c1e4b.

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    french_text = translator.english_to_french(textToTranslate)
-    return french_text
+    # Write your code here
+    return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    english_text = translator.french_to_english(textToTranslate)
-    return english_text
+    # Write your code here
+    return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
-    return render_template('index.html')
+    # Write the code to render template
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
